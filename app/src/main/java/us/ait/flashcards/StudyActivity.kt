55482ceab6a3e9319098b455491dbd8dd5dbb294
dorj222 <dorj222@gmail.com>
2019-12-14 17:02:49 +0100

    edited study mode

--- app/src/main/java/us/ait/flashcards/StudyActivity.kt
+++ app/src/main/java/us/ait/flashcards/StudyActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_study.*
 import us.ait.flashcards.adapter.CardAdapter
 import us.ait.flashcards.adapter.GroupAdapter
-import us.ait.flashcards.adapter.StudyAdapter
 import us.ait.flashcards.data.AppDatabase
 import us.ait.flashcards.data.Card
 import us.ait.flashcards.data.Group
@@ -18,7 +18,7 @@ class StudyActivity : AppCompatActivity() {
         const val KEY_STARTED = "KEY_STARTED"
     }
 
-    lateinit var  studyAdapter: StudyAdapter
+
 
     var groupId :Long = 0
 
@@ -26,68 +26,31 @@ class StudyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study)
 
-        initRecyclerView()
 
         groupId = intent.getLongExtra("KEY_DETAILS", 0)
 
+        var cards = AppDatabase.getInstance(this@StudyActivity).groupDao().getCards(groupId)
+
+
+
+
+        tvAnsw.text = "Question 1"
+
+        tvQuest.text = "Answer 1"
+
+
+        btnPrevious.setOnClickListener {
+
+        }
+
+        btnNext.setOnClickListener {
+
+        }
+
 
     }
 
-    private fun initRecyclerView() {
-        Thread{
-            var cards = AppDatabase.getInstance(this@StudyActivity).groupDao().getCards(groupId)
-
-            runOnUiThread {
-                studyAdapter = StudyAdapter(this, cards)
-                recyclerTodo.adapter = studyAdapter
-
-                var itemDecorator = DividerItemDecoration(
-                    this,
-                    DividerItemDecoration.VERTICAL
-                )
-                recyclerTodo.addItemDecoration(itemDecorator)
 
 
 
-//                val callback = TodoReyclerTouchCallback(todoAdapter)
-//                val touchHelper = ItemTouchHelper(callback)
-//                touchHelper.attachToRecyclerView(recyclerTodo)
-            }
-        }.start()
-    }
-
-    fun showAddCardDialog() {
-        var dialog = CardDialog()
-        var arg = Bundle()
-        arg?.putLong("groupId", groupId)
-        dialog.arguments = arg
-        dialog.show(supportFragmentManager, "TAG_TODO_DIALOG")
-    }
-
-    fun saveCard(card: Card){
-        Thread{
-            var newID =
-                AppDatabase.getInstance(this@StudyActivity).groupDao().addCard(card)
-
-            card.cardId = newID
-
-            runOnUiThread{
-                studyAdapter.addCard(card)
-            }
-        }.start()
-    }
-
-
-//    var editIndex: Int = -1
-
-//    override fun groupUpdated(item: Group) {
-//        Thread{
-//            AppDatabase.getInstance(
-//                this@MainActivity).groupDao().updateGroup(item)
-//
-//            runOnUiThread {
-//                groupAdapter.updateGroupOnPosition(item, editIndex)
-//            }
-//        }.start()
-//    }
 }
